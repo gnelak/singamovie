f340c39e217c40b6d4c3c8b75b027a38f142cb8a
TechLady <kalengzonline@hotmail.com>
2016-09-06 01:30:21 +0800

    Scaffold for Review

--- singa_movie/db/schema.rb
+++ singa_movie/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160905153335) do
+ActiveRecord::Schema.define(version: 20160905171559) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 20160905153335) do
     t.datetime "updated_at",   null: false
     t.integer  "user_id"
     t.string   "image"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
