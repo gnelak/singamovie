206bed1889d6e297dc357e83e324853b2b33c8e0
TechLady <kalengzonline@hotmail.com>
2016-09-06 13:04:15 +0800

    Star ratings for reviews

--- singa_movie/db/schema.rb
+++ singa_movie/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160905171559) do
+ActiveRecord::Schema.define(version: 20160905173103) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20160905171559) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.integer  "movie_id"
   end
 
   create_table "users", force: :cascade do |t|
